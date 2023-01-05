f771b4893b58a373574d7e0cd7d0b5d043e07118
johnbern <gusberjoid@student.gu.se>
2023-01-05 17:00:15 +0100

    Fix test structure to work properly

---
+++ src/tests/user.test.js
@@ -0,0 +1,65 @@
+const db = require('./db')
+const userService = require('../services/user')
+const dentistService = require('../services/dentist')
+const User = require('../models/user')
+const Dentist = require('../models/dentist')
+
+const mongoose = require('mongoose')
+
+//Before tests all the tests, creates and connects to a new test DB
+beforeAll(async () => await db.connect());
+//After each test, clear to database to give each test an empty DB
+afterEach(async () => await db.clearDatabase());
+//When all tests are done we delete the DB and close the connection to mongodb
+afterAll(async () =>  await db.closeDatabase());
+
+describe('userTests', () => {
+    it('createUser', (done) => {
+   //try {
+        console.log('test')
+        userService.createUser('Sam', 'Jobara', 'jultomte', 'jobara@chalmers.se', '7205261234')
+        .then((user) =>{
+        console.log(user)
+        expect(user.first_name).toEqual('Sam')
+        expect(user.last_name).toEqual('Jobara')
+        expect(user.password).toEqual('jultomte')
+        expect(user.email_address).toEqual('jobara@chalmers.se')
+        expect(user.personal_number).toEqual('7205261234')
+        
+        //expect(0).toEqual(1)
+        //console.log('test', e)
+        done()
+        //Promise.resolve()
+        }).catch((err) => {
+            
+        })
+     })
+     /*it('userLogin', (done) => {
+        userService.createUser('Sam', 'Jobara', 'jultomte', 'jobara@chalmers.se', '7205261234')
+        .then(user => {
+        expect(userService.login(user.personal_number, user.password)).toEqual(true)
+        done()
+        })
+     })*/
+})
+
+
+/*
+describe('First Group Of Tests', () => {
+
+    it('First Test', async done => {
+        const result = await numberFunc(10)
+        expect(result.word).toBe("ten")
+        expect(result.number).toBeGreaterThan(10)
+        done()
+
+    })
+    it('Second Test', async done => {
+        const result = await numberFunc()
+        expect(result).toBeNull()
+        done()
+
+    })
+
+})
+*/
