2ad96493580bcc9fe9669b771520aba840411f40
johnbern <gusberjoid@student.gu.se>
2023-01-07 16:52:03 +0100

    Fix method logic

--- src/services/user.js
+++ src/services/user.js
@@ -42,14 +42,15 @@ const login = async (personal_number,password) => {
 
 // Modify a user's password and email
 const modifyUserInfo = async (id, newUser) => {
+    console.log(id, newUser)
     if(id && newUser){
         try{
             const oldUser = await User.find({personal_number: id})
-            
-            if (!oldUser) {
+            console.log(oldUser)
+            if (!oldUser || oldUser.length < 1) {
                 return Promise.reject({ message: 'User does not exist', code: 404 });
             }
-
+            console.log('test')
             const user = await User.findOneAndUpdate(
                 {personal_number: id},
                 {
@@ -58,7 +59,7 @@ const modifyUserInfo = async (id, newUser) => {
                     first_name: newUser.first_name || oldUser.first_name,
                     last_name: newUser.last_name || oldUser.last_name
                 },{new: true},
-            ) 
+            )
             return user;
 
         } catch(e){
