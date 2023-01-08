de6e44e7d073a022692fd36ba57caef448a30f09
johnbern <gusberjoid@student.gu.se>
2023-01-08 11:57:20 +0100

    Add error reason in return topic, shortened code

--- src/services/dentist.js
+++ src/services/dentist.js
@@ -33,12 +33,6 @@ const dentistLogin = async function (username, password) {
 const modifyDentistInfo = async function (id, newDentist) {
     if(id && newDentist){
         try{
-            const oldDentist = await Dentist.find({username: id})
-            
-            if (!oldDentist) {
-                return Promise.reject({ message: 'Dentist does not exist', code: 404 });
-            }
-
             const dentist = await Dentist.findOneAndUpdate(
                 {username: id},
                 {
@@ -48,11 +42,15 @@ const modifyDentistInfo = async function (id, newDentist) {
                     last_name: newDentist.last_name || oldDentist.last_name
                 }, {new: true}
             )
-            return dentist;
+            
+            if (!oldDentist || oldDentist.length < 1) {
+                return Promise.reject({ message: 'Dentist does not exist', code: 404 });
+            }
+             return oldDentist;
 
         } catch(e){
-            console.log(e)
-            return Promise.reject('Malformed dentist data');
+            if (e.code == 11000) return Promise.reject('Username is alreadt in use');
+            else return Promise.reject('Malformed dentist data');
         }
 
     } else {
